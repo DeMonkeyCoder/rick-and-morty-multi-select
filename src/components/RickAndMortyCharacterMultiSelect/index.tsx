57ec631f024b9b3097c3d2c3b372e663fc8383f5
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-04-14 12:15:42 +0330

    refactor: move multi select styles to proper file

--- src/components/RickAndMortyCharacterMultiSelect/index.tsx
+++ src/components/RickAndMortyCharacterMultiSelect/index.tsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { CustomDropdownIndicator } from 'components/RickAndMortyCharacterMultiSelect/CustomDropdownIndicator';
 import { CustomMultiValueRemove } from 'components/RickAndMortyCharacterMultiSelect/CustomMultiValueRemove';
 import { HighlightTextMatch } from 'components/RickAndMortyCharacterMultiSelect/HighlightTextMatch';
+import styles, {
+  fontSize,
+} from 'components/RickAndMortyCharacterMultiSelect/styles';
 import React, { useState } from 'react';
 import { components, GroupBase, OptionProps } from 'react-select';
 import { AsyncPaginate } from 'react-select-async-paginate';
@@ -10,126 +13,6 @@ import {
   RickAndMortyAsyncPaginateProps,
   RickAndMortyCharacter,
 } from 'types';
-
-// Style constants
-const colors = {
-  primary: '#94a3b8',
-  secondary: '#dcdcdc',
-  tertiary: '#e2e8f0',
-  hoverPrimary: '#8392a7',
-  scrollbarThumb: '#c1c1c1',
-  scrollbarThumbHover: '#888',
-  scrollbarBackground: '#fafafa',
-  multiValueBg: '#e2e8f0',
-  multiValueText: '#000',
-  optionSelected: '#DEEBFF',
-  optionActive: '#B2D4FF',
-};
-
-const borderRadius = '14px';
-const fontSize = '16px';
-const padding = '2px';
-const scrollbarWidth = '18px';
-
-// Custom styles using the constants
-const customStyles: RickAndMortyAsyncPaginateProps['styles'] = {
-  input: (provided) => ({
-    ...provided,
-    fontSize,
-  }),
-  valueContainer: (provided, state) => ({
-    ...provided,
-    padding: state.hasValue ? padding : provided.padding,
-  }),
-  control: (provided, state) => ({
-    ...provided,
-    border: state.isFocused
-      ? `1px solid ${colors.primary}`
-      : `1px solid ${colors.secondary}`,
-    boxShadow: state.isFocused
-      ? `0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)`
-      : '',
-    borderRadius,
-    padding,
-    '&:hover': {
-      border: `1px solid ${colors.primary}`,
-    },
-  }),
-  menu: (provided) => ({
-    ...provided,
-    marginTop: '12px',
-    border: `1px solid ${colors.primary}`,
-    boxShadow: 'none',
-    borderRadius,
-  }),
-  menuList: (provided) => ({
-    ...provided,
-    maxHeight: '450px',
-    '::-webkit-scrollbar': {
-      width: scrollbarWidth,
-    },
-    '::-webkit-scrollbar-track': {
-      background: colors.scrollbarBackground,
-      borderRadius: '0px',
-    },
-    '::-webkit-scrollbar-thumb': {
-      background: colors.scrollbarThumb,
-      borderRadius,
-      border: `4px solid ${colors.scrollbarBackground}`,
-    },
-    '::-webkit-scrollbar-thumb:hover': {
-      background: colors.scrollbarThumbHover,
-    },
-  }),
-  multiValue: (styles) => ({
-    ...styles,
-    backgroundColor: colors.multiValueBg,
-    borderRadius,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: '4px',
-    padding: `6px 9px`,
-  }),
-  multiValueLabel: (styles) => ({
-    ...styles,
-    fontSize,
-    color: colors.multiValueText,
-    paddingRight: '6px',
-  }),
-  multiValueRemove: (styles) => ({
-    ...styles,
-    backgroundColor: colors.primary,
-    color: 'white',
-    cursor: 'pointer',
-    borderRadius: '6px',
-    ':hover': {
-      backgroundColor: colors.hoverPrimary,
-      color: 'white',
-    },
-    padding: '6px',
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    cursor: 'pointer',
-    display: 'flex',
-    alignItems: 'center',
-    padding: `12px 6px`,
-    borderBottom: `1px solid ${colors.primary}`,
-    backgroundColor: state.isSelected
-      ? state.isFocused
-        ? colors.optionSelected
-        : 'transparent'
-      : provided.backgroundColor,
-    color: state.isSelected ? 'black' : provided.color,
-    ':active': {
-      ...provided[':active'],
-      backgroundColor: state.isSelected
-        ? colors.optionActive
-        : provided[':active']?.backgroundColor,
-    },
-  }),
-};
 
 const RickAndMortyAsyncPaginate: React.FC<RickAndMortyAsyncPaginateProps> = (
   props,
@@ -241,7 +124,7 @@ const RickAndMortyCharacterMultiSelect = (
         MultiValueRemove: CustomMultiValueRemove,
         IndicatorSeparator: () => null,
       }}
-      styles={customStyles}
+      styles={styles}
       {...props}
     />
   );
---
+++ src/components/RickAndMortyCharacterMultiSelect/styles.ts
@@ -0,0 +1,122 @@
+import { RickAndMortyAsyncPaginateProps } from 'types';
+
+// Style constants
+export const colors = {
+  primary: '#94a3b8',
+  secondary: '#dcdcdc',
+  tertiary: '#e2e8f0',
+  hoverPrimary: '#8392a7',
+  scrollbarThumb: '#c1c1c1',
+  scrollbarThumbHover: '#888',
+  scrollbarBackground: '#fafafa',
+  multiValueBg: '#e2e8f0',
+  multiValueText: '#000',
+  optionSelected: '#DEEBFF',
+  optionActive: '#B2D4FF',
+};
+export const borderRadius = '14px';
+export const fontSize = '16px';
+export const padding = '2px';
+export const scrollbarWidth = '18px';
+
+// Custom styles using the constants
+const styles: RickAndMortyAsyncPaginateProps['styles'] = {
+  input: (provided) => ({
+    ...provided,
+    fontSize,
+  }),
+  valueContainer: (provided, state) => ({
+    ...provided,
+    padding: state.hasValue ? padding : provided.padding,
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    border: state.isFocused
+      ? `1px solid ${colors.primary}`
+      : `1px solid ${colors.secondary}`,
+    boxShadow: state.isFocused
+      ? `0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)`
+      : '',
+    borderRadius,
+    padding,
+    '&:hover': {
+      border: `1px solid ${colors.primary}`,
+    },
+  }),
+  menu: (provided) => ({
+    ...provided,
+    marginTop: '12px',
+    border: `1px solid ${colors.primary}`,
+    boxShadow: 'none',
+    borderRadius,
+  }),
+  menuList: (provided) => ({
+    ...provided,
+    maxHeight: '450px',
+    '::-webkit-scrollbar': {
+      width: scrollbarWidth,
+    },
+    '::-webkit-scrollbar-track': {
+      background: colors.scrollbarBackground,
+      borderRadius: '0px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: colors.scrollbarThumb,
+      borderRadius,
+      border: `4px solid ${colors.scrollbarBackground}`,
+    },
+    '::-webkit-scrollbar-thumb:hover': {
+      background: colors.scrollbarThumbHover,
+    },
+  }),
+  multiValue: (styles) => ({
+    ...styles,
+    backgroundColor: colors.multiValueBg,
+    borderRadius,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: '4px',
+    padding: `6px 9px`,
+  }),
+  multiValueLabel: (styles) => ({
+    ...styles,
+    fontSize,
+    color: colors.multiValueText,
+    paddingRight: '6px',
+  }),
+  multiValueRemove: (styles) => ({
+    ...styles,
+    backgroundColor: colors.primary,
+    color: 'white',
+    cursor: 'pointer',
+    borderRadius: '6px',
+    ':hover': {
+      backgroundColor: colors.hoverPrimary,
+      color: 'white',
+    },
+    padding: '6px',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    padding: `12px 6px`,
+    borderBottom: `1px solid ${colors.primary}`,
+    backgroundColor: state.isSelected
+      ? state.isFocused
+        ? colors.optionSelected
+        : 'transparent'
+      : provided.backgroundColor,
+    color: state.isSelected ? 'black' : provided.color,
+    ':active': {
+      ...provided[':active'],
+      backgroundColor: state.isSelected
+        ? colors.optionActive
+        : provided[':active']?.backgroundColor,
+    },
+  }),
+};
+
+export default styles;
